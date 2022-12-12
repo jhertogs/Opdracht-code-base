c15940f0173e8ccccc1e893967f1c1120b9d6fe2
jhertogs <97103567@st.deltion.nl>
2022-12-12 13:05:15 +0100

    array2

--- geheimschrijver.php
+++ geheimschrijver.php
@@ -1,6 +1,8 @@
 <?php 
 
-array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",);
+$alphabet= array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",);
+$alphabet2=array("z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a",);
+$input=readline();
 
 
 
