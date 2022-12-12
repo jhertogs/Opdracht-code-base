73fee52b45895477ae6b89acd0fc8394acb9d23b
jhertogs <97103567@st.deltion.nl>
2022-12-12 10:46:40 +0100

    added items to array

--- deelbaar.php
+++ deelbaar.php
@@ -1,6 +1,7 @@
 <?php 
 
-array("PHP", "C#", "HTML", "JavaScript", "Java", "C++", "C",)
+$langs= array("PHP", "C#", "HTML", "JavaScript", "Java", "C++", "C",);
+$new_array= array_push($langs, "python", "swift", );
 
 
 ?>
