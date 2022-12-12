44d88a75949c26a7d89b37fa014945b89f140e00
jhertogs <97103567@st.deltion.nl>
2022-12-12 13:58:28 +0100

    removed arrays (better method)

--- geheimschrijver.php
+++ geheimschrijver.php
@@ -1,8 +1,14 @@
 <?php 
+echo "typ";
+$c=readline();
 
-$alphabet= array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",);
-$alphabet2=array("z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a",);
-$input=readline();
+if($c == "z") {
+    $c= "a";
+} else {
+    $c++;
+}
+echo"$c";
+
 
 
 
