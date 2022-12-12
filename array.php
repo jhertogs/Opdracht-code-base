86c41781846cde8b1fc4bcbff15352428857df7d
jhertogs <97103567@st.deltion.nl>
2022-12-12 11:20:01 +0100

    array program finished

--- array.php
+++ array.php
@@ -1,4 +1,24 @@
 <?php 
+//hier word de array met items aangemaakt
+$programmeertalen= array("PHP",
+    "C#", 
+    "HTML",
+    "JavaScript",
+    "Java", 
+    "C++", 
+    "C",
+ );
+//hier worden de items python en swift toegevoegd
+array_push($programmeertalen,
+    "python",
+    "swift",
+ );
+//hier worden de items gesorteerd dan uigeprint en de hoeveelheid items word uitgeprint.
+sort($programmeertalen);
+print_r($programmeertalen);
+echo "de array heeft zoveel items:";
+echo count($programmeertalen);
+
 
 
 ?>
--- deelbaar.php
+++ deelbaar.php
@@ -3,5 +3,12 @@
 $langs= array("PHP", "C#", "HTML", "JavaScript", "Java", "C++", "C",);
 $new_array= array_push($langs, "python", "swift", );
 
+$array= var_dump($new_array);
 
+echo $array;
+
+
+
+ 
+ 
 ?>
