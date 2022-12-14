1246a4a7357d466d5c8170949a3421c153468398
jhertogs <97103567@st.deltion.nl>
2022-12-14 12:52:53 +0100

    first prototype

--- hoofdletters.php
+++ hoofdletters.php
@@ -1,8 +1,16 @@
 <?php 
 
 
-$input=readline("Hallo typ een woord:\n");
+$input=readline("Hallo typ een zin:\n");
 
 $input= strtoupper($input);
+echo "$input \n";
+$input= strtolower($input);
+echo "$input \n";
+$input= ucwords($input);
+echo "$input \n";
+$input= ucfirst($input);
+echo "$input \n";
+
 
 ?>
