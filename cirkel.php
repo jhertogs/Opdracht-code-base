c08c755e5f357667383057ebafa5fcd8c1eb7496
jhertogs <97103567@st.deltion.nl>
2022-12-15 12:33:12 +0100

    improved if statement

--- cirkel.php
+++ cirkel.php
@@ -3,8 +3,7 @@
 echo"hallo voer de diamter van een cirkel in om omtrek/oppervlakte te berekenen (in cm):\n";
 $diameter= readline();
 //checked of input numerisch is en of het lager is dan 0.
-if(is_numeric($diameter)) {
-    if($diameter > 0) {
+if(is_numeric($diameter) && $diameter > 0 ) {
         //de calculaties cirkel
         $omtrek= $diameter * M_PI;
         $straal= $diameter / 2;
@@ -16,7 +15,6 @@ if(is_numeric($diameter)) {
         echo "de berekende omtrek: $omtrek cm\n";
         echo "de berekende oppervlakte: $oppervlakte cm²\n";
         exit; 
-    }
 }
 //als input niet een numerische heeft of lager is dan 0: 
 echo "INVALID INPUT\n";
