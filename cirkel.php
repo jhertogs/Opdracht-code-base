aa75874e95b8d0a89b082de5f3d5e0bdfe0bbb5f
jhertogs <97103567@st.deltion.nl>
2022-12-15 10:13:36 +0100

    added text

--- cirkel.php
+++ cirkel.php
@@ -1,22 +1,24 @@
 <?php 
-
+//readline
 echo"hallo voer de diamter van een cirkel in om omtrek/oppervlakte te berekenen (in cm):\n";
 $diameter= readline();
-//checkd 
+//checked of input numerisch is en of het lager is dan 0.
 if(is_numeric($diameter)) {
     if($diameter > 0) {
-
+        //de calculaties cirkel
         $omtrek= $diameter * M_PI;
         $straal= $diameter / 2;
         $oppervlakte= ($straal * $straal) * M_PI;
-
+        //omtrek & oppervlakte worden afgrerond op 2 decimalen.
         $omtrek= round($omtrek,2);
         $oppervlakte= round($oppervlakte,2);
+        //de omtrek & oppervlakte worden geprint.
         echo "de berekende omtrek: $omtrek cm\n";
         echo "de berekende oppervlakte: $oppervlakte cm²\n";
         exit; 
     }
 }
+//als input niet een numerische heeft of lager is dan 0: 
 echo "INVALID INPUT\n";
 
 ?>
